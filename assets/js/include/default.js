e679db218343cd67f9d1d58d1032f9e1984926bc
Diamond_ <diamondautomated@gmail.com>
2022-09-19 15:12:22 +0900

    Create default.js

---
+++ assets/js/include/default.js
@@ -0,0 +1,29 @@
+function head() {
+  $.ajax({
+      url: "/assets/html/head/en.html",
+      cache: false,
+      success: function(html){
+          document.write(html);
+      }
+  });
+}
+
+function header() {
+  $.ajax({
+      url: "/assets/html/header/default.html",
+      cache: false,
+      success: function(html){
+          document.write(html);
+      }
+  });
+}
+
+function footer() {
+  $.ajax({
+      url: "/assets/html/footer/en.html",
+      cache: false,
+      success: function(html){
+          document.write(html);
+      }
+  });
+}
